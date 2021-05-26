4d983f424caa64e56be3645097968b359c528616
99keshav99 <47789950+99keshav99@users.noreply.github.com>
2021-05-26 22:37:18 +0530

    changes to size limit and expiration date
    changed size limit from 50kb to 5mb
    changed expiration date from 30 days to 365 days

--- src/config.js
+++ src/config.js
@@ -1,10 +1,10 @@
 module.exports = {
-	SIZE_LIMIT: 50, // mentioned in KB
+	SIZE_LIMIT: 5000, // mentioned in KB
 	PORT: process.env.PORT || 3000,
 	MONGO_URL: process.env.MONGODB_URI || "mongodb+srv://root:root@jsonbox-clone-cluster.gtohm.mongodb.net/jsonboxDB?retryWrites=true&w=majority",
 	REQUEST_LIMIT_PER_HOUR: 100,
-	ENABLE_DATA_EXPIRY: true, // Once switched on the index will be be set in mongodb. Might need to remove it in order to switch off the behaviour
-	DATA_EXPIRY_IN_DAYS: 30,
+	ENABLE_DATA_EXPIRY: false, // Once switched on the index will be be set in mongodb. Might need to remove it in order to switch off the behaviour
+	DATA_EXPIRY_IN_DAYS: 365,
 	FILTER_IP_SET: [],  // example ['172.29.0.1']
 	FILTER_OPTIONS: { mode: 'allow' },
 };
